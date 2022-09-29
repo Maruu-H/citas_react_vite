2501bf1c05f52e29ccd361e91d3d8aae58394d34
Maruu-H <mauraruminothernandez@gmail.com>
2022-09-29 17:19:44 -0300

    Primer Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports = {
+//   purge: ["./index.html", "./src/**/*.jsx"],
+//   darkMode: false,
+//   theme: {
+//     extend: {},
+//   },
+//   variants:{
+//     extends: {}
+//   },
+//   plugins: [],
+// }
